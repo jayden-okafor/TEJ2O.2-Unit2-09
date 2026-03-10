3ece67ab639a96b9ff4635c1fcb25abc56e4ae31
jayden-okafor <jayden.okafor@stu.ocsb.ca>
2026-03-10 17:28:06 -0400

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,66 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2026 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
-*/
+ * Created by: Jayden
+ * Created on: Mar 2026
+ * This program chooses between rock, paper or scissors when you shake the microbit. 
+ */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number = null
+let score: number = null
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    // generate random number between 0 and 2
+    randomNumber = randint(0, 2)
+
+    // clear screen
+    basic.clearScreen()
+
+    // if the randomised number is 0 then show rock
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+
+    // if the randomised number is 1 then show paper
+    if (randomNumber == 1) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    // if the randomised number is 2 then show scissors
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+
+})
+
+// when the "a" button is pressed
+input.onButtonPressed(Button.A, function () {
+    // clear screen
+    basic.clearScreen()
+
+    // add 1 to the current score value
+    score += 1
+
+    // show checkmark
+    basic.showIcon(IconNames.Yes)
+
+    // wait for 1 second
+    basic.pause(1000)
+
+    // show happy face
+    basic.showIcon(IconNames.Happy)
+
+})
+
+// when the "b" button is pressed
+input.onButtonPressed(Button.B, function () {
+    // clear clearScreen
+    basic.clearScreen()
+
+    // show the score
+    basic.showString(score.toString())
+
+})
